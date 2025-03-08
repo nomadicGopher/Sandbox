25a31c6d7e9332ed756840d6867a12cfce30f89b
Jon <11096910+nomadicGopher@users.noreply.github.com>
2025-03-07 17:51:40 -0700

    learn about interfaces, types, nullish coalescing operators & ternary operators. Document bug in node dist/main.js

---
+++ ts/dist/functions.js
@@ -0,0 +1,4 @@
+'use strict';
+export function initWithTsIgnore(localParam) {
+    console.log(localParam);
+}
---
+++ ts/src/functions.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+export function initWithTsIgnore(localParam : string) {
+  console.log(localParam);
+ }
