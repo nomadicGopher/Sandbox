6cc4c68d3d6cfe46e04d863a99e3ad467a2cf277
Jon Vojtush <11096910+nomadicGopher@users.noreply.github.com>
2025-03-30 22:10:39 -0600

    update go/wasm

--- go/wasm(stdGoCompiler)/build.sh
+++ go/wasm(stdGoCompiler)/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-LOG_FILE="app.log"
+LOG_FILE="build.log"
 touch $LOG_FILE 2>&1 | tee /dev/stderr
-echo "Script started at: $(date)" | tee -a $LOG_FILE
+
+echo "----------------------------------------------------------------------------------------------------" | tee -a $LOG_FILE
+echo "Build process started at: $(date)" | tee -a $LOG_FILE
 
 echo "Removing web/go.wasm if it exists..." | tee -a $LOG_FILE
 rm -f web/go.wasm 2>&1 | tee -a $LOG_FILE >&2
 
-echo "Building a new file..." | tee -a $LOG_FILE
+echo "Building a new web/go.wasm..." | tee -a $LOG_FILE
 GOOS=js GOARCH=wasm go build -o=web/go.wasm -buildvcs=false main.go 2>&1 | tee -a $LOG_FILE >&2
 status=$?
 if [ $status -ne 0 ]; then
@@ -15,14 +17,13 @@ if [ $status -ne 0 ]; then
 fi
 echo "web/go.wasm was built." | tee -a $LOG_FILE
 
-echo "Removing web/wasm_exec.js if it exists..." | tee -a $LOG_FILE
-rm -f web/wasm_exec.js 2>&1 | tee -a $LOG_FILE >&2
+if [ ! -f web/wasm_exec.js ]; then
+    echo "Fetching a new web/wasm_exec.js..." | tee -a $LOG_FILE 
+    cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" web/ 2>&1 | tee -a $LOG_FILE >&2  
+    echo "web/wasm_exec.js was fetched from \$GOROOT/misc/wasm/" | tee -a $LOG_FILE
+else
+    echo "web/wasm_exec.js already exists. Skipping fetch." | tee -a $LOG_FILE
+fi
 
-echo "Fetching a new file..." | tee -a $LOG_FILE 
-cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" web/ 2>&1 | tee -a $LOG_FILE >&2  
-echo "web/wasm_exec.js was fetched from \$GOROOT/misc/wasm/" | tee -a $LOG_FILE
-
-echo "Script ended at: $(date)" | tee -a $LOG_FILE
-echo "----------------------------------------------------------------------------------------------------" | tee -a $LOG_FILE
-
+echo "Build process ended at: $(date)" | tee -a $LOG_FILE
 exit
