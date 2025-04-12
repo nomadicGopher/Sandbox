f1eef9e64f54584998640ade47ce4dafb9c152b2
Jon Vojtush <11096910+nomadicGopher@users.noreply.github.com>
2025-04-12 00:22:30 -0400

    set handlers & ssl

--- go/html-template+htmx/main.go
+++ go/html-template+htmx/main.go
@@ -3,28 +3,65 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+	"strconv"
 )
 
-type PageData struct {
-	Title string
+type Window struct {
+	PageTitle string
+}
+
+func convertPortsToStrings(httpsPort *uint, httpPort *uint) (httpsPortStr string, httpPortStr string) {
+	httpsPortStr = strconv.Itoa(int(*httpsPort))
+	httpPortStr = strconv.Itoa(int(*httpPort))
+	return
+}
+
+func setHandlers() {
+	http.HandleFunc("/", func(responseWriter http.ResponseWriter, request *http.Request) {
+		windowData := Window{PageTitle: "Hello World with Go and HTMX"}
+		index := template.Must(template.ParseFiles("./index.html"))
+		index.Execute(responseWriter, windowData)
+	})
+
+	http.HandleFunc("/update", func(responseWriter http.ResponseWriter, request *http.Request) {
+		updateFilePath := "templates/update.html"
+		updateContent, err := os.ReadFile(updateFilePath)
+		if err != nil {
+			http.Error(responseWriter, "Unable to read "+updateFilePath, http.StatusInternalServerError)
+			return
+		}
+		responseWriter.Write(updateContent)
+	})
 }
 
 func main() {
-	// Parse HTML template
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	ssl := flag.Bool("ssl", false, "Enable/disable SSL layer for the server.")
+	certFile := flag.String("cert", "server.crt", "Path to the SSL certificate file.")
+	keyFile := flag.String("key", "server.key", "Path to the SSL key file.")
+	httpsPort := flag.Uint("https", 443, "Port to serve over for HTTPS server.")
+	httpPort := flag.Uint("http", 80, "Port to serve over for HTTP server.")
+	flag.Parse()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		data := PageData{Title: "Hello World with Go and HTMX"}
-		tmpl.Execute(w, data)
-	})
+	setHandlers()
 
-	// Endpoint for HTMX interaction
-	http.HandleFunc("/update", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<p><strong>Dynamic Update:</strong> Hello from HTMX!</p>"))
-	})
+	httpsPortStr, httpPortStr := convertPortsToStrings(httpsPort, httpPort)
 
-	// Start server
-	http.ListenAndServe(":8080", nil)
+	if *ssl {
+		fmt.Println("HTTPS server starting at http://localhost:", httpsPortStr)
+		err := http.ListenAndServeTLS(":"+httpsPortStr, *certFile, *keyFile, nil)
+		if err != nil {
+			fmt.Println("Error starting HTTPS server:", err)
+		}
+	} else {
+		fmt.Println("HTTP server starting at http://localhost:", httpPortStr)
+		err := http.ListenAndServe(":"+httpPortStr, nil)
+		if err != nil {
+			fmt.Println("Error starting HTTP server:", err)
+		}
+	}
 }
