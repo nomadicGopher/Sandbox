195fe99ec0dbe8b6e3b07b08e782439560fea52a
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-07-11 14:11:06 +0000

    Create sample.go
    Signed-off-by: nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>

---
+++ go/data-sources/mongoDB/sample.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "go.mongodb.org/mongo-driver/mongo"
+    "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+    // Set client options
+    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+
+    // Connect to MongoDB
+    client, err := mongo.Connect(context.TODO(), clientOptions) // TODO
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Check the connection
+    err = client.Ping(context.TODO(), nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("Connected to MongoDB!")
+
+    // Disconnect the client
+    err = client.Disconnect(context.TODO())
+    if err != nil {
+        log.Fatal(err)
+    }
+}
