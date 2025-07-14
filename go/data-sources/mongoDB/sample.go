4c8c7beeaf7270b43e50bee0187ccc6f4d50ba2d
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-07-14 10:52:59 -0400

    mod

--- go/data-sources/mongoDB/sample.go
+++ go/data-sources/mongoDB/sample.go
@@ -1,34 +1,35 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-    // Set client options
-    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	// Set client options
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-    // Connect to MongoDB
-    client, err := mongo.Connect(context.TODO(), clientOptions) // TODO
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions) // TODO
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Check the connection
-    err = client.Ping(context.TODO(), nil)
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Println("Connected to MongoDB!")
+	fmt.Println("Connected to MongoDB!")
 
-    // Disconnect the client
-    err = client.Disconnect(context.TODO())
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Disconnect the client
+	err = client.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
