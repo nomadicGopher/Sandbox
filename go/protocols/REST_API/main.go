945cc7c2b22916bdef876e44fa4b4a9469fd0dc6
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-09-25 13:49:15 -0600

    how to levrage servemux for multiple servers

--- go/protocols/REST_API/main.go
+++ go/protocols/REST_API/main.go
@@ -1,47 +1,62 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	mux1 := http.NewServeMux()
+	mux2 := http.NewServeMux()
 
-	mux = setHandlers(mux)
+	mux1.HandleFunc("GET /users", usersHandler)
+	mux1.HandleFunc("GET /users/", usersSubtreeHandler)
+	mux1.HandleFunc("PUT /users/{id}", usersIDHandler)
+	mux1.HandleFunc("PUT /users/jon", usersJonHandler)
+	mux1.HandleFunc("GET /", rootHandler)
 
-	http.ListenAndServe(":9999", mux)
+	mux2.HandleFunc("GET /users", usersHandler)
+	mux2.HandleFunc("GET /users/", usersSubtreeHandler)
+	mux2.HandleFunc("PUT /users/{id}", usersIDHandler)
+	mux2.HandleFunc("PUT /users/jon", usersJonHandler)
+	mux2.HandleFunc("GET /", rootHandler)
+
+	// Start both servers on different ports
+	go func() {
+		log.Println("Starting mux1 on :9999")
+		http.ListenAndServe(":9999", mux1)
+	}()
+	log.Println("Starting mux2 on :8888")
+	http.ListenAndServe(":8888", mux2)
 }
 
-func setHandlers(mux *http.ServeMux) *http.ServeMux {
-	mux.HandleFunc("GET /users", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Query().Get("page") != "" {
-			// pass ...?page=# in the request URL
-			w.Write([]byte("users response page: " + r.URL.Query().Get("page")))
-		} else {
-			w.Write([]byte("users endpoint"))
-		}
-	})
+// Handler Functions
 
-	mux.HandleFunc("GET /users/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("users subtree endpoint"))
-	})
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("page") != "" {
+		// pass ...?page=# in the request URL
+		w.Write([]byte("users response page: " + r.URL.Query().Get("page")))
+	} else {
+		w.Write([]byte("users endpoint"))
+	}
+}
 
-	mux.HandleFunc("PUT /users/{id}", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("users dynamic path id: " + r.PathValue("id")))
-	})
+func usersSubtreeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("users subtree endpoint"))
+}
 
-	mux.HandleFunc("PUT /users/jon", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("specific endpoint is prioritized"))
-	})
+func usersIDHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("users dynamic path id: " + r.PathValue("id")))
+}
 
-	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.NotFound(w, r)
-			return
-		}
+func usersJonHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("specific endpoint is prioritized"))
+}
 
-		w.Write([]byte("root endpoint"))
-	})
-
-	return mux
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("root endpoint"))
 }
