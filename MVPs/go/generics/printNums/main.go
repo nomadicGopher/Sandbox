c5a1b5e026dcdedd2013241eb2f54eecc6a395b6
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-11-06 20:15:43 -0700

    improve type assignments

--- MVPs/go/generics/printNums/main.go
+++ MVPs/go/generics/printNums/main.go
@@ -10,13 +10,13 @@ func checkEmptyNumber[T int | float64](v *T) string {
 		return ""
 	}
 
-	vType := reflect.TypeOf(*v).Kind() // can also use .String() to compare against == "string" for example
+	vType := reflect.TypeOf(*v).Kind() // can also use .String() to compare against == "string"
 
 	switch vType {
 	case reflect.Int:
-		return fmt.Sprintf("%v", *v)
+		return fmt.Sprintf("%d", *v)
 	case reflect.Float64:
-		return fmt.Sprintf("%v", *v)
+		return fmt.Sprintf("%.2f", *v)
 	}
 
 	return "Logic Error"
@@ -28,7 +28,7 @@ func main() {
 	result1 := checkEmptyNumber(input1Ptr)
 	fmt.Printf("Int value: %s\n", result1)
 
-	input2 := 1.11
+	input2 := 1.00
 	input2Ptr := &input2
 	result2 := checkEmptyNumber(input2Ptr)
 	fmt.Printf("Float value: %s\n", result2)
