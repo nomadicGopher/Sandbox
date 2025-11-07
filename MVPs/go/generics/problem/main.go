ad21eb97f403bb1d59e88d6fb97ef16b67d1b783
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-11-06 20:27:49 -0700

    solve

--- MVPs/go/generics/problem/main.go
+++ MVPs/go/generics/problem/main.go
@@ -6,9 +6,9 @@ import (
 
 // Cache structure using Go Generics
 type Cache[K comparable, V any] struct {
-	capacity int
-	store    map[K]*list.Element
-	evict    *list.List
+	capacity int                 // Maximum number of items the cache can hold
+	store    map[K]*list.Element // Maps keys to their corresponding list element for fast lookup
+	evict    *list.List          // Doubly-linked list to track eviction order (LRU: least recently used at the back)
 }
 
 // Entry structure to hold key-value pairs in the cache
@@ -27,13 +27,42 @@ func NewCache[K comparable, V any](capacity int) *Cache[K, V] {
 }
 
 func (c *Cache[K, V]) Put(key K, value V) {
-	// TODO
+	if elem, ok := c.store[key]; ok {
+		// Update value and move to front
+		elem.Value.(*entry[K, V]).value = value
+		c.evict.MoveToFront(elem)
+		return
+	}
+
+	// Evict if necessary
+	if len(c.store) >= c.capacity {
+		if backElem := c.evict.Back(); backElem != nil {
+			evictEntry := backElem.Value.(*entry[K, V])
+			delete(c.store, evictEntry.key)
+			c.evict.Remove(backElem)
+		}
+	}
+
+	// Insert new entry
+	newEntry := &entry[K, V]{key: key, value: value}
+	elem := c.evict.PushFront(newEntry)
+	c.store[key] = elem
 }
 
 func (c *Cache[K, V]) Get(key K) (V, bool) {
-	// TODO
+	var zero V
+	if elem, ok := c.store[key]; ok {
+		c.evict.MoveToFront(elem)
+		return elem.Value.(*entry[K, V]).value, true
+	}
+	return zero, false
 }
 
 func (c *Cache[K, V]) Remove(key K) bool {
-	// TODO
+	if elem, ok := c.store[key]; ok {
+		c.evict.Remove(elem)
+		delete(c.store, key)
+		return true
+	}
+	return false
 }
